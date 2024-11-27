e459e6a6047810685f375fa047427da3bd07bf51
Pestica Mariyasiluvaimichal <pmariyasiluvaimichal@freshworks.com>
2024-11-27 15:47:07 +0530

    small changes

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ document.addEventListener('click', function (event) {
         dropdown.classList.toggle('show');
     }
 });
+[
+    {
+      "image": "./assets/images/beach.jpg",
+      "altText": "India Tour",
+      "title": "India Tour Packages",
+      "tours": 0
+    },
+    {
+      "image": "./assets/images/boy_pic.jpg",
+      "altText": "International Tour",
+      "title": "International Tour Packages",
+      "tours": 0
+    },
+    {
+      "image": "./assets/images/kerala.jpg",
+      "altText": "Honeymoon Package",
+      "title": "International Honeymoon Packages",
+      "tours": 0
+    },
+    {
+      "image": "./assets/images/beach.jpg",
+      "altText": "Europe Tour",
+      "title": "Europe Tour Packages",
+      "tours": 0
+    },
+    {
+      "image": "./assets/images/cable.webp",
+      "altText": "Educational Tour",
+      "title": "Educational Tour Packages",
+      "tours": 0
+    }
+  ]

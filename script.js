2396173920c8cc828bdc7e7ae319776281fb23fc
Pestica Mariyasiluvaimichal <pmariyasiluvaimichal@freshworks.com>
2024-12-17 17:49:58 +0530

    change

--- script.js
+++ script.js
@@ -21,59 +21,6 @@ document.addEventListener('click', function (event) {
     }
 });
 
-const data = [
-    "Taj Mahal, Agra",
-    "Jaipur, Rajasthan",
-    "Varanasi, Uttar Pradesh",
-    "Goa Beaches",
-    "Kerala Backwaters",
-    "Leh-Ladakh",
-    "Rishikesh, Uttarakhand",
-    "Hampi, Karnataka"
-];
-
-const naturalPlaces = [
-    "Backwaters of Kerala",
-    "Valley of Flowers, Uttarakhand",
-    "Dal Lake, Srinagar",
-    "Great Rann of Kutch, Gujarat",
-    "Sundarbans, West Bengal",
-    "Munnar, Kerala",
-    "Andaman and Nicobar Islands",
-    "Coorg, Karnataka"
-];
-
-const adventurePlaces = [
-    "Leh-Ladakh, Jammu and Kashmir",
-    "Rishikesh (for River Rafting), Uttarakhand",
-    "Jim Corbett National Park, Uttarakhand",
-    "Gir National Park, Gujarat",
-    "Kaziranga National Park, Assam",
-    "Spiti Valley, Himachal Pradesh",
-    "Ziro Valley, Arunachal Pradesh",
-    "Manali, Himachal Pradesh"
-];
-
-const spiritualPlaces = [
-    "Varanasi, Uttar Pradesh",
-    "Rameswaram, Tamil Nadu",
-    "Golden Temple, Amritsar",
-    "Bodh Gaya, Bihar",
-    "Tirupati, Andhra Pradesh",
-    "Dwarka, Gujarat",
-    "Jagannath Temple, Puri",
-    "Haridwar, Uttarakhand"
-];
-
-const beachPlaces = [
-    "Goa Beaches",
-    "Pondicherry Beaches",
-    "Gokarna, Karnataka",
-    "Varkala, Kerala",
-    "Marina Beach, Chennai",
-    "Radhanagar Beach, Andaman",
-    "Alleppey, Kerala"
-];
 function searchFunction() {
     const input = document.getElementById("searchInput").value.toLowerCase();
     const resultsList = document.getElementById("searchResults");
